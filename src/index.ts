d4ec783584860d4ea18620b5ba93815217b2d1cb
Troy Bohrer <=>
2024-02-27 14:15:45 -0500

    checking commit status

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,5 @@
 const list = document.querySelector<HTMLUListElement>("#list");
 const form = document.querySelector<HTMLFormElement>("#new-task-form");
 const input = document.querySelector<HTMLInputElement>("#new-task-title");
+
+console.log(list)

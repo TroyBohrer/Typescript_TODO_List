34abfb0f86e5b811d4a9cecb99850958a7cb19d9
Troy Bohrer <tbohrer96@duck.com>
2024-02-27 14:30:54 -0500

    created event listener

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
-// import { v4 as uuidV4 } from 'uuid';
-
-// console.log(uuidV4())
+import { v4 as uuidV4 } from 'uuid';
 
 const list = document.querySelector<HTMLUListElement>("#list");
-const form = document.querySelector<HTMLFormElement>("#new-task-form");
+const form = document.getElementById("#new-task-form") as HTMLFormElement || null;
 const input = document.querySelector<HTMLInputElement>("#new-task-title");
 
-console.log(list)
+form?.addEventListener("submit", e => {
+  e.preventDefault()
+
+  if (input?.value == "" || input?.value == null) return;
+
+  const task = {
+    id: uuidV4(),
+    title: input.value,
+    completed: false,
+    createdAt: new Date(),
+  }
+})

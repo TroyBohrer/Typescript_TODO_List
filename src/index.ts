67eb7ca8c9a252753891bfdb9fa2bc0ec7e10d02
Troy Bohrer <tbohrer96@duck.com>
2024-02-27 15:49:54 -0500

    completed functionality, tasks saved to localStorage

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,58 @@
-import { v4 as uuidV4 } from 'uuid';
+import { v4 as uuidV4 } from "uuid";
+
+type Task = {
+  id: string;
+  title: string;
+  completed: boolean;
+  createdAt: Date;
+};
 
 const list = document.querySelector<HTMLUListElement>("#list");
-const form = document.getElementById("#new-task-form") as HTMLFormElement || null;
+const form =
+  (document.getElementById("new-task-form") as HTMLFormElement) || null;
 const input = document.querySelector<HTMLInputElement>("#new-task-title");
+const tasks: Task[] = loadTasks();
+tasks.forEach(addTaskItem);
 
-form?.addEventListener("submit", e => {
-  e.preventDefault()
+form?.addEventListener("submit", (e) => {
+  e.preventDefault();
 
   if (input?.value == "" || input?.value == null) return;
 
-  const task = {
+  const newTask: Task = {
     id: uuidV4(),
     title: input.value,
     completed: false,
     createdAt: new Date(),
-  }
-})
+  };
+
+  tasks.push(newTask);
+
+  addTaskItem(newTask);
+  input.value = "";
+});
+
+function addTaskItem(task: Task) {
+  const item = document.createElement("li");
+  const label = document.createElement("label");
+  const checkbox = document.createElement("input");
+  checkbox.addEventListener("change", () => {
+    task.completed = checkbox.checked;
+    saveTasks();
+  });
+  checkbox.type = "checkbox";
+  checkbox.checked = task.completed;
+  label.append(checkbox, task.title);
+  item.append(label);
+  list?.append(item);
+}
+
+function saveTasks() {
+  localStorage.setItem("Tasks", JSON.stringify(tasks));
+}
+
+function loadTasks(): Task[] {
+  const taskJSON = localStorage.getItem("Tasks");
+  if (taskJSON == null) return [];
+  return JSON.parse(taskJSON);
+}
